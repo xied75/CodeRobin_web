176b044dc53d18ceb56c3bd97c4487ca580cfa52
Dong Xie <xied75@gmail.com>
2014-08-24 22:28:51 +0100

    can't get it work, bypass test

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,7 +67,7 @@ module.exports = function (grunt) {
         connect: {
             options: {
                 port: 9000,
-                open: true,
+                open: false,
                 livereload: 35729,
                 // Change this to '0.0.0.0' to access the server from outside
                 hostname: '127.0.0.1'
